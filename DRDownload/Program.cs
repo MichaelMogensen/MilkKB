91a0a5311a4431c4b16ea7a2479ecc6af6c878b8
Michael Mogensen <michael-mogensen-danmark@hotmail.com>
2026-03-28 10:29:10 +0100

    Download works

--- DRDownload/Program.cs
+++ DRDownload/Program.cs
@@ -4,7 +4,10 @@ using DRDownload.Video;
 Console.WriteLine("Downloading radio/video broadcasts...");
 
 //await new DRRadio().DownloadRadioBroadcastsAsync();
-await new DRVideo().DownloadVideoBroadcastsAsync();
+//await new DRVideo().DownloadVideoBroadcastsAsync();
+
+
+await new DRVideo().DownloadAsync();
 
 Console.WriteLine("Done...");
 
--- DRDownload/Video/DRVideo.cs
+++ DRDownload/Video/DRVideo.cs
@@ -1,5 +1,10 @@
 ﻿using DRDownload.Common;
 using DRDownload.Common.Types;
+using FFMpegCore;
+using FFMpegCore.Arguments;
+using FFMpegCore.Enums;
+using System.Net.Sockets;
+using System.Resources;
 
 namespace DRDownload.Video
 {
@@ -11,17 +16,45 @@ namespace DRDownload.Video
 
         public async Task DownloadVideoBroadcastsAsync()
         {
-            var url = new DRUrlVideo("0_ela5z5u2").Url;
+            //var url = new DRUrlVideo("0_ela5z5u2").Url;
+            //var file = new LocalVideoFilename(
+            //    @"c:\temp",
+            //    new BroadcastMetadata(
+            //        "Lørdagshjørnet - Otto Leisner",
+            //        new DateTime(2014, 4, 19, 17, 58, 0),
+            //        new TimeSpan(0, 49, 7),
+            //        "DR-K",
+            //        "Aftenens gæst i Lørdagshjørnet er Otto Leisner")).File;
+            //await DownloadStreamAsync(url, file);
+
+            var url = new DRUrlVideo("0_tsmswb48").Url;
             var file = new LocalVideoFilename(
                 @"c:\temp",
                 new BroadcastMetadata(
-                    "Lørdagshjørnet - Otto Leisner",
-                    new DateTime(2014, 4, 19, 17, 58, 0),
-                    new TimeSpan(0, 49, 7),
-                    "DR-K",
-                    "Aftenens gæst i Lørdagshjørnet er Otto Leisner")).File;
+                    "Dansk Naturgas kavalkade",
+                    new DateTime(1987, 12, 31, 23, 10, 0),
+                    TimeSpan.FromMinutes(45),
+                    "DR1",
+                    "What did ju have in jur tæjsk today")).File;
             await DownloadStreamAsync(url, file);
         }
+
+        class ProtocolWhitelistArgument : IArgument
+        {
+            public string Text => "-protocol_whitelist file,http,https,tcp,tls,crypto";
+        }
+
+        public async Task DownloadAsync()
+        {
+            await FFMpegArguments
+                .FromFileInput(@"C:\Temp\Otto.m3u", true, op => op
+                    .WithArgument(new ProtocolWhitelistArgument())
+                    )
+                .OutputToFile(@"C:\Temp\Otto3.mp4", true, op => op
+                    .WithFastStart())
+                .ProcessAsynchronously();
+        }
+
     }
 }
 
