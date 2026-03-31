7aab77e7483fae8434f1145cb3522d6eec41f245
Michael Mogensen <michael-mogensen-danmark@hotmail.com>
2026-03-31 11:32:40 +0200

    Prepare JSON

---
+++ DRDownload/Common/Types/Broadcast/Broadcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DRDownload.Common.Types.Broadcast
+{
+    public class Broadcast
+    {
+        public string? EntityId { get; set; }
+        public string? Title { get; set; }
+        public DateTime SendDate { get; set; }
+        public int DurationMin { get; set; }
+        public string? Channel { get; set; }
+        public string? ExtraInfo { get; set; }
+    }
+}
+
